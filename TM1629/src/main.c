ee608f56410c3ffca5ad6555b0dcfe3f3ae12e24
A.Amani <didi1364@gmail.com>
2022-10-06 13:32:04 +0330

    feat: print message using serial port

--- TM1629/src/main.c
+++ TM1629/src/main.c
@@ -1,1 +1,22 @@
-void app_main() {}
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_system.h"
+#include "driver/spi_master.h"
+#include "sdkconfig.h"
+#include "driver/uart.h"
+void app_main() 
+{
+
+
+    while (1)
+    {
+    printf("Tick\n");
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+    
+}
